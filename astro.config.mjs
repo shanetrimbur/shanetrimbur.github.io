e229c77db014d95348682d5a46666c627ba155c8
shanetrimbur <shanetrimbur@gmail.com>
2025-02-03 09:17:46 -0800

    Update site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shanetrimbur.github.io',
+  base: '/',
   integrations: [tailwind()],
 });
